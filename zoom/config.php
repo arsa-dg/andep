18efcad8c4fa96ebb26853a13f7eda6c852ff54b
ronggurmwp <ronggur.mwp@gmail.com>
2020-08-07 23:38:44 +0700

    nambah zoom(blm selesai)

---
+++ zoom/config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+ 
+define('CLIENT_ID', 'KBhM8cIaRPu5sUE5Gu_uw');
+define('CLIENT_SECRET', 'p4eEBMq0uBPPIF2kFiXdlRVx33D9qdTv');
+define('REDIRECT_URI', 'https://5caa70a5b442.ngrok.io/zoom/callback.php');
